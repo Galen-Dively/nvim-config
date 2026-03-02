d2a2dc279811e06daf0ea25314c0170ab8d6c520
Galen Dively <galen.dively4@gmail.com>
2026-03-02 15:33:56 -0500

    Added Nvim-Tree for file exploring

--- init.lua
+++ init.lua
@@ -1,7 +1,20 @@
+-- Settings for nvim-tree that need to be at begining according to github
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+
 require("config.lazy")
 vim.cmd.colorscheme("habamax")
+
+-- Relative Line Numbers
 vim.wo.relativenumber = true
+
+-- Tab Spacing
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.expandtab = true
+
+-- nvim--tree cont
+vim.opt.termguicolors = true
+require("nvim-tree").setup()
