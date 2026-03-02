387d8b595160f35a8ddbb699d1adb0dbc17c2c59
Galen-Dively <65838051+Galen-Dively@users.noreply.github.com>
2026-03-02 18:31:57 -0500

    Added keymaps for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,5 +3,11 @@ return {
 		"nvim-telescope/telescope.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
     cmd = "Telescope",
+    opts = {},
+    keys = {
+      {"<leader>ff", "<cmd>Telescope find_files<cr>", mode = "n", desc = "Find Files"},
+      {"<leader>fg", "<cmd>Telescope live_grep<cr>", mode = "n", desc="Live Grep"},
+      {"<leader>fb", "<cmd> Telescope buffers<cr>", mode = "n", desc = "Find Buffers"},
+    }
 	}
 }
